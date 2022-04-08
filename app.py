de768008324c2e88bbd3ec083a1a605897e7652f
Hasan <99032434+neinsteffen@users.noreply.github.com>
2022-04-08 15:34:16 +0300

    Updated

--- app.py
+++ app.py
@@ -1,72 +1,59 @@
-from cv2 import QT_RADIOBOX
 from pynput import keyboard
 from PyQt5.QtWidgets import QMainWindow, QApplication,QGraphicsDropShadowEffect,QDesktopWidget
-from PyQt5.QtCore import Qt
-from groupBox import Ui_Form
-from PyQt5 import QtGui,QtWidgets
-from pynput.mouse import Button, Controller
-from pynput import keyboard
-class Open(QMainWindow):
-    
+from page import Open
+class Load(object):
     def __init__(self):
-        super(Open, self).__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
-        self.show()
-        self.setPageLocation()
-
-    def setPageLocation(self):
-        mouse = Controller()
-        current_mouse_position = mouse.position
-        av = QDesktopWidget().availableGeometry()
-        sc = QDesktopWidget().screenGeometry()
-
-        widget = self.geometry()
-        x = av.width() - widget.width()
-        y = 2 * sc.height() - sc.height() - widget.height()
-        self.move(current_mouse_position[0],current_mouse_position[1])
-
-
+        super(Load, self).__init__()
+      
+    def execute(self):
+        pass
+        #self.loading = Open()
+        #self.loading.show()
         
 
 
-if __name__ == '__main__':
+    
+    
+    
 
-    #Bu kısımda ileride şarta koyacağımız tuş kombinasyonunu belirtiyoruz
-    current = set()
+if __name__ == '__main__':
+    
+    
     COMBS = [
         {keyboard.Key.tab, keyboard.KeyCode(char='x')},
         {keyboard.Key.tab, keyboard.KeyCode(char='X')}
     ]
-
-    def run(c):
-        #programı ayağa kaldıran kısım
-        print(" e: ", c)
-        import sys
-        app = QApplication(sys.argv)
-        load = Open()
-        load.show()
-        app.exit(app.exec_())
-    
-   
-    def onPress(key):
-        #eğer key bizim kombinimizde belirtilenlerin içinde varsa
+    current = set()
+    def run(key):
         if any([key in multi for multi in COMBS]):
-             #key olayları gerçeleştiğinde bunları hafızaya alıyoruz
+            #key olayları gerçekleştiğinde bunları hafızaya alıyoruz
             current.add(key)
         if any(all(k in current for k in multi) for multi in COMBS):
-            #eğer 2 key durumu da gerçekleşirse programı ayağa kaldıracak fonksiyonu çağrıyoruz
-            run(current)
-            print(current)
-
+            import sys
+            app = QApplication(sys.argv)
+            load = Open()
+            load.show()
+            app.exit(app.exec_())
+            
+            
+            
     def onRelease(key):
-        #her seferinde aynı key olayı gelmesindiye eklenen keyleri siliyoruz
         if any([key in multi for multi in COMBS]):
             current.remove(key)
         if key == keyboard.Key.esc:
-            return False 
-    with keyboard.Listener(on_press=onPress, on_release=onRelease) as listener:
-        listener.join() #joinleme
+            return False     
+    with keyboard.Listener(on_press=run, on_release=onRelease) as listener:
+        
+        listener.join()
+    
+        
+    
+     
+    
+    
+    
+
+
 
     
 
---
+++ page.py
@@ -0,0 +1,29 @@
+from cv2 import QT_RADIOBOX
+from PyQt5.QtWidgets import QMainWindow, QApplication,QGraphicsDropShadowEffect,QDesktopWidget
+from PyQt5.QtCore import Qt
+from groupBox import Ui_Form
+from PyQt5 import QtGui,QtWidgets
+from pynput.mouse import Button, Controller
+
+class Open(QMainWindow):
+    
+    def __init__(self):
+        super(Open, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+        self.show()
+        self.setPageLocation()
+
+    def setPageLocation(self):
+        mouse = Controller()
+        current_mouse_position = mouse.position
+        av = QDesktopWidget().availableGeometry()
+        sc = QDesktopWidget().screenGeometry()
+
+        widget = self.geometry()
+        x = av.width() - widget.width()
+        y = 2 * sc.height() - sc.height() - widget.height()
+        self.move(current_mouse_position[0],current_mouse_position[1])
+
+
+            
