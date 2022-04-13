1cea674677d5bda103596bb98e7af467c831e7c4
Hasan <99032434+neinsteffen@users.noreply.github.com>
2022-04-13 03:01:57 +0300

    Updated-5

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 
-from ast import Set
-from typing import List
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5 import QtCore
 
+from PyQt5.QtWidgets import QMainWindow, QMessageBox,QApplication
+#from PyQt5 import QtWidgets
+from PyQt5 import QtCore,QtGui
 from page import Open
 from mainWindow import Ui_MainWindow
 from PyQt5.Qt import Qt
 import keyboard,pyperclip,time
-
-
 
 class Main(QMainWindow):
     def __init__(self):
@@ -18,20 +14,30 @@ class Main(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
         ###FUNCS
         self.ui.open_button.clicked.connect(self.open)
         self.ui.close_button.clicked.connect(lambda: self.shutdown_p())
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.ui.btn_close.clicked.connect(self.close_a)
+        self.ui.btn_minimize.clicked.connect(lambda: self.minimized_p())
         
-
+        def moveWindow(event):
+            if event.buttons() == QtCore.Qt.LeftButton:
+                self.move(self.pos() + event.globalPos()- self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+        self.ui.label.mouseMoveEvent = moveWindow
     
+    def minimized_p(self):
+        self.showMinimized()
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
     def selected_item(self, isItem):
-       
         pyperclip.copy(isItem.text())
-        #print(isItem.text())
-      
-        
 
+        self.loader.close()    
     def shutdown_p(self):
         reply = QMessageBox.question(self, 'Window Close', 'Are you sure you want to close the window?', QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
@@ -39,27 +45,25 @@ class Main(QMainWindow):
                 print('Window closed')
         else:
             pass
-
+    def close_a(self):
+        self.close()
 
     def open(self):
         #Giving information to user
-        self.ui.lbl_info.setText('Running')
-        self.ui.open_button.setStyleSheet("background-color : green")
-        self.ui.close_button.setStyleSheet("background-color : midlight")
-
-        print("listener working")
-        #if program is run, the process will execute
-        if self.ui.lbl_info.text() == "Running":
+            self.ui.lbl_info.setText('Running')
+            self.ui.open_button.setStyleSheet("background-color : green")
+            self.ui.close_button.setStyleSheet("background-color : midlight")
+            print("listener working")
+            #if program is run, the process will execute
             #start another thread
             self.listener = Listener()
+            
             self.listener.start()
             self.listener.finished.connect(self.listenerFinished)
             self.listener.val_bool.connect(self.showPage)
             self.listener.arr.connect(self.showPage)
    
     def showPage(self, val):
-        
-        #self.list = set()
         print("show page val type:", type(val))
         if type(val) == set:
             #collect copyied texts to show list in new window
@@ -67,25 +71,24 @@ class Main(QMainWindow):
             for txt in val:
                 self.loader.ui.showList.addItem(txt)
                 count += 1
-            #val.remove(txt)
             self.loader.ui.showList.itemClicked.connect(self.selected_item)                  
             
         if type(val) == bool and val == True:
-            self.loader = Open()          
+            self.loader = Open()    
             self.loader.show()
             self.loader.activateWindow()
             
         
-        
     def listenerFinished(self):
+        self.listener = Listener()
         print("Listener Done!")
-        self.listener.exit()
+        self.listener.kill()
 
 
-class Listener(QThread): #Thread
+class Listener(QtCore.QThread): #Thread
     copyiedVal =  set()
-    val_bool = pyqtSignal(bool)
-    arr = pyqtSignal(set)
+    val_bool = QtCore.pyqtSignal(bool)
+    arr = QtCore.pyqtSignal(set)
     text = " "
     list_ = set()
     def run(self):
@@ -98,23 +101,16 @@ class Listener(QThread): #Thread
                 
             if keyboard.is_pressed("ctrl+b"):
                 self.val_bool.emit(True)
-                self.arr.emit(self.list_)
-                
-
+                self.arr.emit(self.list_)           
             time.sleep(0.1)
 
 
-
-
-    
-
-
 if __name__ == '__main__':
-    import sys
-    app = QApplication(sys.argv)
+    from sys import exit,argv
+    app = QApplication(argv)
     main = Main()
     main.show()
-    sys.exit(app.exec())
+    exit(app.exec())
     
    
                       
--- page.py
+++ page.py
@@ -1,10 +1,9 @@
-from cv2 import QT_RADIOBOX
+
 from PyQt5.QtWidgets import QMainWindow, QApplication,QGraphicsDropShadowEffect,QDesktopWidget
 from PyQt5.QtCore import Qt
 from groupBox import Ui_Form
-from PyQt5 import QtGui,QtWidgets
-from pynput.mouse import Button, Controller
-
+from pynput.mouse import Controller
+from PyQt5 import QtCore,QtGui
 class Open(QMainWindow):
     
     def __init__(self):
@@ -13,7 +12,18 @@ class Open(QMainWindow):
         self.ui.setupUi(self)
         self.show()
         self.setPageLocation()
-
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.ui.btn_close.clicked.connect(lambda: self.shutdown_p())
+        self.ui.btn_minimize.clicked.connect(lambda: self.minimized_p())
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
+                    
+        
+    
+    def shutdown_p(self):
+        self.close()
+    def minimized_p(self):
+        self.showMinimized()
     def setPageLocation(self):
         mouse = Controller()
         current_mouse_position = mouse.position
@@ -24,6 +34,8 @@ class Open(QMainWindow):
         x = av.width() - widget.width()
         y = 2 * sc.height() - sc.height() - widget.height()
         self.move(current_mouse_position[0],current_mouse_position[1])
+
+    
 
 
 
